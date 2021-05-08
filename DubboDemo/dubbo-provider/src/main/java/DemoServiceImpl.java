9fbcbc305cb337f3b8e33df77b0569c3757d9501
wuyujie <2633963595@qq.com>
2021-05-08 17:27:35 +0800

    项目初始化1

---
+++ DubboDemo/dubbo-provider/src/main/java/DemoServiceImpl.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+public class DemoServiceImpl implements DemoService {
+    public List<String> getPermissions(Long id) {
+        List<String> demo = new ArrayList<String>();
+        demo.add(String.format("Permission_%d", id - 1));
+        demo.add(String.format("Permission_%d", id));
+        demo.add(String.format("Permission_%d", id + 1));
+        return demo;
+    }
+}
